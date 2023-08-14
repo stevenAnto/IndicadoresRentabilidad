2d270a209b9df2ff0794dc349c481d5dc8199081
stevenAnto <ecalcinap@unsa.edu.pe>
2023-08-13 19:09:56 -0500

    add save rpt

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
 
 var userFullName = '';
 var userKey = '';
-var  contador = 0;
+var  contador = 1;
+var contadorProyectos=0;
 function showFormVPN(){
   let formhtml ='<h2>Valor Presente Neto (VPN).</h2>-<br><p> Se trata de la diferencia entre el valor de mercado de una inversión y su costo. Este indicador de rentabilidad mide cuánto valor es creado por realizar cierta inversión. Para evaluar un proyecto de inversión con base en el Valor Presente Neto se tienen que considerar aspectos como inversión inicial previa, tasa de descuento, número de períodos y flujos netos de efectivo</p>'
-  formhtml += `<div class="ContainerMed">
+  formhtml += `<div class= "ContainerMedGeneral" id="ContainerMedGeneral">
+              <div class="containerTabla">
+              <p>Inversion Inicial : -1000</p>
+              <p>Tasa de descuento : 2% </p>
+              <table>
+              <tr>
+              <th>Inversion</th>
+              <th colspan="5">Flujos de Efectivo</th>
+              </tr>
+              <tr >
+              <th>0</th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              </tr >
+              <tr class="tercercaFila">
+              <th class="uno">Inversion</th>
+              <th class="dos">Ingresos-Costos</th>
+              <th class="tres">Ingresos-Costos</th>
+              <th class="cuatro">Ingresos-Costos</th>
+              <th class="cinco">Ingresos-Costos</th>
+              <th class="seis">Ingresos-Costos</th>
+              </tr>
+              <tr>
+              <td>-1000</td>
+              <td>200</td>
+              <td>300</td>
+              <td>100</td>
+              <td>200</td>
+              <td>500</td>
+              </tr>
+              </table>
+<p>VPN=&minus;1000 + 200 &divide (1.02)<sup>1</sup> +
+  300&divide(1.02)<sup>2</sup></sub> +
+  100&divide(1.02)<sup>3</sup></sub> +
+  200&divide(1.02)<sup>4</sup></sub> +
+  500&divide(1.02)<sup>5</sup></sub>
+</p>
+<p>VPN=216.30</p>
+              </div>
+              <div class="ContainerMed">
               <div class="inputGroup">
               <label>Inversion Inicial</label>
               <div class="inputMasSimbols">
@@ -26,6 +69,8 @@ function showFormVPN(){
               <div class="inputMasSimbols">
               <input type="text" id="flujo" name ="anios" placeholder="(Ingresos - Gastos)">
               <div>S/ </div>
+              <i class="fa-regular fa-rectangle-xmark"></i>
+
               </div>
               </div>
               </div>
@@ -41,9 +86,21 @@ function showFormVPN(){
               <button onclick ="CalcularVPN()">Calcular </button>
               <button onclick ="GrabarVPN()">Grabar para comparar </button>
               <p id="mensaje" ></p>
+    </div>
+    <div id="respuestasGrabadas"></div>
     </div>`;
   document.getElementById('main').innerHTML = formhtml;
 
+}
+function GrabarVPN(){
+  contadorProyectos++;
+  let contenedor = document.getElementById("respuestasGrabadas");
+  let respuesta = document.getElementById("rptName");
+  let nuevoResultado = document.createElement("p");
+  nuevoResultado.textContent="Proyecto "+contadorProyectos+
+    ": " +respuesta.value;
+  nuevoResultado.style.whiteSpace="pre-line";
+  contenedor.appendChild(nuevoResultado);
 }
 function CalcularVPN(){
   let respuesta;
@@ -52,15 +109,16 @@ function CalcularVPN(){
   let suma=0;
   const objetoFlujos = document.getElementById("flujosCaja");
 
-  for(let i = 0;i<=contador; i++){
+  for(let i = 1;i<=contador; i++){
     let tasa  =tasaDescuento/100;
-    let valor = parseFloat(objetoFlujos.children[i].children[1].children[0].value);
-    let valorActualizado = valor/(1+tasa)**(i+1);
+    let valor = parseFloat(objetoFlujos.children[i-1].children[1].children[0].value);
+    let valorActualizado = valor/(1+tasa)**(i);
     suma +=valorActualizado;
     console.log("Valor",i,valorActualizado);
   }
   console.log(suma);
   respuesta = -inversionInicial + suma;
+  respuesta = respuesta.toFixed(2);
   let tagRespuesta = document.getElementById("rptName")
   console.log(tagRespuesta);
   tagRespuesta.value = respuesta;
@@ -87,8 +145,16 @@ function showprc(){
   document.getElementById('main').innerHTML = formhtml;
 
 }
+function eliminarFlujo(){
+  const objetoFlujos = document.getElementById("flujosCaja");
+  objetoFlujos.lastElementChild.remove();
+
+}
 function agregarAnio(){
   contador++;
+  let icono = document.createElement("i");
+  icono.setAttribute("class","fa-regular fa-rectangle-xmark")
+  icono.addEventListener("click",eliminarFlujo);
   const nuevoDivContainer = document.createElement("div")
   nuevoDivContainer.setAttribute("class","inputGroup")
   const nuevoLabel = document.createElement("label")
@@ -102,6 +168,7 @@ function agregarAnio(){
   nuevoDivContainer.appendChild(nuevoLabel)
   nuevoDivGroup.appendChild(nuevoInput)
   nuevoDivGroup.appendChild(nuevoDivS)
+  nuevoDivGroup.appendChild(icono)
   nuevoDivContainer.appendChild(nuevoDivGroup)
   const aquien = document.getElementById('flujosCaja');
   aquien.appendChild(nuevoDivContainer);
